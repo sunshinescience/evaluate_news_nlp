dfe58acbb6bb59087b482c0d0ca00fe01a858a48
Sunshine Abbott <sunshine.abbott@gmail.com>
2020-04-04 13:19:29 +0100

    refactored

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 
+// Add an entry as the starting point where webpack will begin to build our dependency tree from
 module.exports = {
     entry: './src/client/index.js',
 };
