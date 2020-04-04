b7e0cc80f649ea96fa7c1077d2192bb157aaa37b
Sunshine Abbott <sunshine.abbott@gmail.com>
2020-04-04 09:51:31 +0100

    install Aylien credentials

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Set aylien API credentials
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+    });
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
