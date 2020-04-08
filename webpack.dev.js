611368fc0cc3d1f2be8c2f4a68c7a7868bdcce1b
Sunshine Abbott <sunshine.abbott@gmail.com>
2020-04-08 13:44:32 +0100

    in the package.json file - changed the code to have a build-prod and build-dev instead of build. And, I deleted the webpack.config.js file but inserted a webpack.dev.js and a webpack.prod.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,5 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
