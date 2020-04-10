3545144e18c03a587871cc76615cc96a9055c291
Sunshine Abbott <sunshine.abbott@gmail.com>
2020-04-10 10:48:43 +0100

    imported more Sass files into index.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -17,7 +17,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ] // Note: chained loaders run from right to left, so in this case, the sass-loader would be the first loader to run, and the css-loader would be the second, and the style loader would be the last
             }
         ]
     },
