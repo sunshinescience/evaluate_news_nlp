4d4e492a1652eb105190ae382704d0659080050a
Sunshine Abbott <sunshine.abbott@gmail.com>
2020-04-15 14:37:33 +0100

    adding service workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,12 +4,13 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     mode: 'production',
     entry: './src/client/index.js',
     optimization: {
-        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
     },
     module: {
         rules: [
@@ -31,6 +32,7 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({ 
             filename: "[name].css" 
-        })
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
