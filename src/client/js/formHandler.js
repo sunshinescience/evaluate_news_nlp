a922cf4cdc2c8e8d58a50a84f4c0bffaef9a650a
Sunshine Abbott <sunshine.abbott@gmail.com>
2020-04-27 21:05:46 +0100

    trying to get fetch working

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,8 +3,16 @@ function handleSubmit(event) {
 
     console.log("handleSubmit entered");
 
-    fetch("http://localhost:8080/test").then(res => res.json()).then(function(res) {console.log(res)});
+    //getData("http://localhost:8080/test");
 
+    fetch('http://localhost:8080/test')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    });
+    
     //postData("http://localhost:8080/add").then(function(res) {console.log(res)});
 
     /*
@@ -41,6 +49,21 @@ function handleSubmit(event) {
     console.log("handleSubmit done");
 } 
 
+const getData = async (url = '') => { 
+    //console.log(url);
+    console.log("started getData");
+    
+    try {
+        const response = await fetch(url);
+        console.log("awaited fetch");
+        //const newData = await response.json();
+        //console.log(response);
+        //return response;
+    } catch(error) {
+        console.log('error', error);
+    };
+};
+
 const postData = async (url = '', data = {}) => { 
     const response = await fetch(url, {
         method: 'POST', 
@@ -59,7 +82,6 @@ const postData = async (url = '', data = {}) => {
     } catch(error) {
         console.log('error', error);
     };
-    
 };
 
 
